31317fbf542f6548f790ce34d9db8c4d6ca840c2
Shashank Sapre <shashank.sapre@obconnect.io>
2024-11-18 12:34:32 +0530

    multiple pings

--- app/main.js
+++ app/main.js
@@ -5,7 +5,11 @@ console.log("Logs from your program will appear here!");
 
 // Uncomment this block to pass the first stage
 const server = net.createServer((connection) => {
-  connection.write("+PONG\r\n");
+  connection.on("data", (data) => {
+    if (data.toString() == "*1\r\n$4\r\nPING\r\n") {
+      connection.write("+PONG\r\n");
+    }
+  });
 });
 
 server.listen(6379, "127.0.0.1");
