9062bc9926cfdfa309a7889a398ba3a923925251
Shashank Sapre <shashank.sapre@obconnect.io>
2024-11-28 12:04:32 +0530

    codecrafters submit [skip ci]

--- app/handler.js
+++ app/handler.js
@@ -1,5 +1,25 @@
 let store = [];
-let stream = [];
+
+/**
+ * @typedef {Object} KeyValue
+ * @property {string} key - The key name
+ * @property {string} value - The corresponding value
+ */
+
+/**
+ * @typedef {Object} StreamEntry
+ * @property {string} streamId - The ID of the stream (e.g., "0-1")
+ * @property {KeyValue[]} streamData - Array of key-value pairs in the stream
+ */
+
+/**
+ * @typedef {Object} Stream
+ * @property {string} streamKey - The identifier for the stream (e.g., "apple")
+ * @property {StreamEntry[]} stream - Array of stream entries
+ */
+
+/** @type {Stream[]} */
+let streams = [];
 
 export function requestHandler(data, config) {
   const splitData = data.toString().split("\r\n");
@@ -38,7 +58,7 @@ export function requestHandler(data, config) {
     case "type":
       const typeSearch = splitData[4];
       const pair = store.find((data) => data.key === typeSearch);
-      const str = stream.find((data) => data.streamKey === typeSearch);
+      const str = streams.find((data) => data.streamKey === typeSearch);
       if (pair) {
         return [typeof pair.value];
       } else if (str) {
@@ -49,25 +69,40 @@ export function requestHandler(data, config) {
     case "xadd":
       const streamKey = splitData[4];
       const streamId = splitData[6];
+      const stream = streams.find((data) => data.streamKey === streamKey);
       const streamData = [];
       for (let i = 8; i < splitData.length; i = i + 4) {
         streamData.push({ key: splitData[i], value: splitData[i + 2] });
       }
-      const sk = stream.find((data) => data.streamKey === streamKey);
-      if (!sk) {
-        stream.push({ streamKey, stream: { streamId, streamData } });
+      const splitId = streamId.split("-");
+      if (splitId[0] == 0 && splitId[1] == 0) {
+        return "E:GreaterThan0-0";
+      }
+      if (!stream) {
+        streams.push({ streamKey, stream: [{ streamId, streamData }] });
       } else {
-        stream = stream
-          .filter((s) => s.streamKey !== streamKey)
-          .push({
-            streamKey,
-            stream: [...sk.stream, { streamId, streamData }],
-          });
+        const lastIdSplit =
+          stream.stream[stream.stream.length - 1].streamId.split("-");
+        const newIdSplit = streamId.split("-");
+        if (lastIdSplit[0] > newIdSplit[0]) {
+          return "E:GreaterThanPrevious";
+        }
+        if (
+          lastIdSplit[0] == newIdSplit[0] &&
+          lastIdSplit[1] >= newIdSplit[1]
+        ) {
+          return "E:GreaterThanPrevious";
+        }
+        streams = streams.filter((s) => s.streamKey !== streamKey);
+        streams.push({
+          streamKey,
+          stream: [...stream.stream, { streamId, streamData }],
+        });
       }
       return streamId;
     default:
       console.log(`Received: ${data.toString()}`);
-      return "E";
+      return "E:0";
   }
 }
 
@@ -82,8 +117,16 @@ export function responseHandler(response, conn) {
     case "NULL":
       conn.write("$-1\r\n");
       break;
-    case "E":
+    case "E:0":
       conn.write("-ERR unknown command\r\n");
+      break;
+    case "E:GreaterThan0-0":
+      conn.write("-ERR The ID specified in XADD must be greater than 0-0\r\n");
+      break;
+    case "E:GreaterThanPrevious":
+      conn.write(
+        "-ERR The ID specified in XADD is equal or smaller than the target stream top item\r\n"
+      );
       break;
     default:
       if (Array.isArray(response)) {
