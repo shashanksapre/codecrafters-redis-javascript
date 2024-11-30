a354b661e1f03b410aef3a4b6196e43f39819103
Shashank Sapre <shashank.sapre@outlook.com>
2024-12-01 03:51:05 +0530

    codecrafters submit [skip ci]

--- app/services/store.js
+++ app/services/store.js
@@ -40,8 +40,48 @@ export function rdbFileToStore(rdbFile) {
   while (i < rdbFile.length && rdbFile[i] != 0xff) {
     if (rdbFile[i] === 0xfd) {
       // Save data to store with expiry in seconds
+      i++;
+      let expirationBuffer = Buffer.alloc(4);
+      for (let x = 3; x >= 0; x--) {
+        expirationBuffer[x] = rdbFile[i];
+        i++;
+      }
+      i++; // skip type
+      const key = getNextBytes(i, rdbFile);
+      i = key.offset;
+      const value = getNextBytes(i, rdbFile);
+      i = value.offset;
+      store.data.push({
+        key: key.nextBytes.toString(),
+        value: value.nextBytes.toString(),
+      });
+      const timeout = Number(expirationBuffer.toString()) - Date.now() / 1000;
+      console.log(key, timeout);
+      setTimeout(() => {
+        store.data = store.data.filter((d) => d.key != key);
+      }, timeout);
     } else if (rdbFile[i] === 0xfc) {
       // Save data to store with expiry in milliseconds
+      i++;
+      let expirationBuffer = Buffer.alloc(8);
+      for (let x = 7; x >= 0; x--) {
+        expirationBuffer[x] = rdbFile[i];
+        i++;
+      }
+      i++; // skip type
+      const key = getNextBytes(i, rdbFile);
+      i = key.offset;
+      const value = getNextBytes(i, rdbFile);
+      i = value.offset;
+      store.data.push({
+        key: key.nextBytes.toString(),
+        value: value.nextBytes.toString(),
+      });
+      const timeout = Number(expirationBuffer.toString()) - Date.now();
+      console.log(key, timeout);
+      setTimeout(() => {
+        store.data = store.data.filter((d) => d.key != key);
+      }, timeout);
     } else {
       // Save data to store
       i++; // Skipping type check for now.
